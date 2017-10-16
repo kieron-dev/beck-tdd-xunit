af9cf35f100053a75766e6faa85923220f463dad
Kieron Browne <kieron.browne@gmail.com>
2017-10-16 16:05:05 +0200

    tearDown() runs after test method

--- test_case_test.py
+++ test_case_test.py
@@ -2,16 +2,11 @@ from test_case import TestCase
 from was_run import WasRun
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
 
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
--- was_run.py
+++ was_run.py
@@ -2,8 +2,10 @@ from test_case import TestCase
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log += "tearDown "
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
